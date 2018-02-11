052be01c0ca0ce4d1cfa23e5026c1675c7aff782
Flavien-Pensato <flavien.pensato@gmail.com>
2018-02-11 12:41:30 +0100

    chore(lint): implement m6web linter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
-const App = () => <div>Hello</div>;
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+const App = () => (
+  <Router>
+    <div className="app">
+      <div className="app__menu">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+      </div>
+
+      <div className="app__content">
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </div>
+  </Router>
+);
 
 export default App;
