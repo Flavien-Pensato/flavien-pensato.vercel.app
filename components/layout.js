e03b5a8e7ae14c12b325af209b21140bbd9d5aa5
Flavien Pensato <Flavien-Pensato@users.noreply.github.com>
2019-01-05 12:03:12 +0100

    chore(): add linter (#27)
    * chore(): add linter
    
    * fixup! chore(): add linter

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
 
 import { Contacts } from './contacts.component';
 import { Title } from './title.component';
@@ -9,16 +9,16 @@ export const Layout = ({ children, className }) => (
   <main className={className}>
     <Head>
       <title>Flavien Pensato</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="msapplication-TileColor" content="#00aba9" />
-      <meta name="theme-color" content="#ffffff"></meta>
-      <meta property="og:image" content="/static/favicons/og-image.jpg"></meta>
-      <meta property="og:image:height" content="209"></meta>
-      <meta property="og:image:width" content="400"></meta>
-      <meta property="og:title" content="An awesome page"></meta>
+      <meta name="theme-color" content="#ffffff" />
+      <meta property="og:image" content="/static/favicons/og-image.jpg" />
+      <meta property="og:image:height" content="209" />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:title" content="An awesome page" />
       <meta property="og:description" content="Everything you need to know about the topic you are looking for" />
-      <meta property="og:url" content="https://flavien-pensato.github.io/"></meta>
+      <meta property="og:url" content="https://flavien-pensato.github.io/" />
       <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
@@ -27,7 +27,12 @@ export const Layout = ({ children, className }) => (
     </Head>
 
     <Title />
-    {children}
+    {React.Children.only(children)}
     <Contacts />
   </main>
-)
+);
+
+Layout.propTypes = {
+  children: PropTypes.element.isRequired,
+  className: PropTypes.string.isRequired,
+};
