1a48911bd1dbaa00eef3bce86a60a5b63c3f55c0
Flavien-Pensato <flavien.pensato@gmail.com>
2019-01-05 12:14:12 +0100

    feat(): add more meta

--- components/layout.js
+++ components/layout.js
@@ -16,9 +16,10 @@ export const Layout = ({ children, className }) => (
       <meta property="og:image" content="/static/favicons/og-image.jpg" />
       <meta property="og:image:height" content="209" />
       <meta property="og:image:width" content="400" />
-      <meta property="og:title" content="An awesome page" />
-      <meta property="og:description" content="Everything you need to know about the topic you are looking for" />
+      <meta property="og:title" content="Homepage Flavien Pensato" />
+      <meta property="og:description" content="Hey, I'm Flavien Pensato. I'm a 24 years old boys working as a Frontend developer. Always bet on Javascript" />
       <meta property="og:url" content="https://flavien-pensato.github.io/" />
+      <meta name="Description" content="Hey, I'm Flavien Pensato. I'm a 24 years old boys working as a Frontend developer. Always bet on Javascript" />
       <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
--- pages/_document.js
+++ pages/_document.js
@@ -1,31 +1,32 @@
-
-import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps (ctx) {
-    const sheet = new ServerStyleSheet()
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
 
-    const originalRenderPage = ctx.renderPage
+    const originalRenderPage = ctx.renderPage;
     ctx.renderPage = () => originalRenderPage({
-      enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-    })
+      enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+    });
 
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps, styles: [...initialProps.styles, ...sheet.getStyleElement()] }
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return { ...initialProps, styles: [...initialProps.styles, ...sheet.getStyleElement()] };
   }
 
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
-          <link href="https://unpkg.com/nes.css@0.0.2/css/nes.min.css" rel="stylesheet" />
+          <link rel="preload" href="https://unpkg.com/nes.css@0.0.2/css/nes.min.css" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
