65aed6a1fcba565b139ce8c5e149dd6f6c13a1dc
f-pensato <flavien.pensato@gmail.com>
2019-01-21 17:14:40 +0100

    fixup! fixup! fixup! feat(mario): add mario

--- modules/game/caracter.js
+++ modules/game/caracter.js
@@ -10,54 +10,58 @@ export const directions = {
 export const defaultCaracter = (positionX = 32, positionY = 0) => ({
   X: positionX,
   Y: positionY,
-  gravity: 0.01,
-  gravitySpeed: 0,
-  gravityMax: 0.08,
-  motion: 0.006,
-  motionSpeed: 0,
-  motionMax: 0.08,
+  gravity: {
+    value: 0,
+    speed: 0.01,
+    max: 0.08,
+  },
+  motion: {
+    value: 0,
+    speed: 0.006,
+    max: 0.08,
+  },
   direction: directions.right,
 });
 
 export const update = (caracter, controls, canvasHeight, delta) => {
   if (controls[keys.up]) {
-    caracter.gravitySpeed = -caracter.gravityMax;
-    caracter.Y += caracter.gravitySpeed * delta;
+    caracter.gravity.value = -caracter.gravity.max;
+    caracter.Y += caracter.gravity.value * delta;
   }
 
   if (controls[keys.right] || controls[keys.left]) {
-    caracter.motionSpeed += caracter.motion;
+    caracter.motion.value += caracter.motion.speed;
 
-    if (caracter.motionSpeed >= caracter.motionMax) {
-      caracter.motionSpeed = caracter.motionMax;
+    if (caracter.motion.value >= caracter.motion.max) {
+      caracter.motion.value = caracter.motion.max;
     }
 
     if (controls[keys.left]) {
       caracter.direction = directions.left;
-      caracter.X -= caracter.motionSpeed * delta;
+      caracter.X -= caracter.motion.value * delta;
     } else {
       caracter.direction = directions.right;
-      caracter.X += caracter.motionSpeed * delta;
+      caracter.X += caracter.motion.value * delta;
     }
   } else {
-    caracter.motionSpeed = 0;
+    caracter.motion.value = 0;
   }
 
   // if (controls[keys.down]) {}
 
-  caracter.gravitySpeed += caracter.gravity;
+  caracter.gravity.value += caracter.gravity.speed;
 
-  if (caracter.gravitySpeed >= caracter.gravityMax) {
-    caracter.gravitySpeed = caracter.gravityMax;
+  if (caracter.gravity.value >= caracter.gravity.max) {
+    caracter.gravity.value = caracter.gravity.max;
   }
 
-  caracter.Y += caracter.gravitySpeed * delta;
+  caracter.Y += caracter.gravity.value * delta;
 
   const rockbottom = canvasHeight;
 
   if (caracter.Y > rockbottom) {
     caracter.Y = rockbottom;
-    caracter.gravitySpeed = 0;
+    caracter.gravity.value = 0;
   }
 
   return caracter;
