3ef5a86c2c7beb7bf4daa99a4b737d6266cad6a8
Flavien Pensato <Flavien-Pensato@users.noreply.github.com>
2020-08-30 20:41:54 +0200

    fix(): upgrade emotion (#56)

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Document, { Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+import { extractCritical } from 'emotion-server'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-
-    const originalRenderPage = ctx.renderPage;
-
-    ctx.renderPage = () => originalRenderPage({
-      enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-    });
-
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return { ...initialProps, styles: [...initialProps.styles, ...sheet.getStyleElement()] };
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = extractCritical(initialProps.html)
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style data-emotion-css={styles.ids.join(' ')} dangerouslySetInnerHTML={{ __html: styles.css }} />
+        </>
+      ),
+    }
   }
 
   render() {
@@ -41,6 +41,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </html>
-    );
+    )
   }
 }
