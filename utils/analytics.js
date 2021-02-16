cbff345bbe8c335de9a7782c406d0d3b535ce0df
Flavien-Pensato <flavien.pensato@gmail.com>
2021-02-16 20:14:21 +0100

    fix(): react ga

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,7 +1,8 @@
 import ReactGA from "react-ga";
 
 export const initGA = () =>
-  process.env.REACT_GA && ReactGA.initialize(process.env.REACT_GA);
+  process.env.NEXT_PUBLIC_REACT_GA &&
+  ReactGA.initialize(process.env.NEXT_PUBLIC_REACT_GA);
 
 export const logPageView = () => {
   ReactGA.pageview(window.location.pathname + window.location.search);
