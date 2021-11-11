f3348646825ed334eb650833c7405603aafa0b70
Flavien-Pensato <flavien.pensato@gmail.com>
2021-11-11 20:49:25 +0100

    ci(): setup typescript

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,3 @@
-import React from "react";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 import { slugBlogs, getMetaBlogFromSlug } from "../utils/blog";
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,3 @@
-import React, { useEffect } from "react";
-import Router from "next/router";
-
 import "../styles/global";
 
 const MyApp = ({ Component, pageProps }) => {
