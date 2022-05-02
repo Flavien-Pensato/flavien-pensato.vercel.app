ec73c108bcddbc0f87c9c47fb9de70ca0f21594e
Flavien-Pensato <flavien.pensato@gmail.com>
2022-05-02 23:32:33 +0200

    feat[]: add todo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,8 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: <React.Fragment>{initialProps.styles}</React.Fragment>,
-    };
+
+    return initialProps;
   }
 
   render() {
