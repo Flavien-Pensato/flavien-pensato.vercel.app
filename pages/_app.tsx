b124b44bf3e8ea3293c1236a3cf1dfb253586f8c
Flavien PENSATO <flavien.pensato.ext@homeserve.fr>
2024-04-08 15:06:36 +0200

    feat(venus): switch to vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,12 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
-import { initGA, logPageView } from "../utils/analytics";
-
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/global.css";
 
 const MyApp = ({ Component, pageProps }) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    initGA();
-
-    if (!router.asPath.includes("?")) {
-      logPageView();
-    }
-  }, []);
-
-  useEffect(() => {
-    router.events.on("routeChangeComplete", logPageView);
-
-    return () => {
-      router.events.off("routeChangeComplete", logPageView);
-    };
-  }, [router.events]);
-
-  return <Component {...pageProps} />;
+	return (
+		<Analytics>
+			<Component {...pageProps} />
+		</Analytics>
+	);
 };
 
 export default MyApp;
