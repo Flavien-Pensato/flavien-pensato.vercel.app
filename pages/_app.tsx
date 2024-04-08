4b9e645f7850a7c11da36fcef882ea7f7324330b
Flavien PENSATO <flavien.pensato.ext@homeserve.fr>
2024-04-08 15:45:26 +0200

    feat(): move analitycs to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/global.css";
 
 const MyApp = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	);
 };
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { Analytics } from "@vercel/analytics/react";
 
 class MyDocument extends Document {
 	static async getInitialProps(ctx) {
@@ -22,7 +21,6 @@ class MyDocument extends Document {
 				<body>
 					<Main />
 					<NextScript />
-					<Analytics />
 				</body>
 			</Html>
 		);
