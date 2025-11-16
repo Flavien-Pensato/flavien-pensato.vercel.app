bc3427d034d0fca8a8ba9844af193dcffe236453
Flavien Pensato <flavien.pensato@gmail.com>
2025-11-16 15:41:15 +0100

    feat: use private server

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withMDX = require("@next/mdx")({
 
 module.exports = withMDX({
   pageExtensions: ["ts", "tsx", "mdx", "tsx"],
+  experimental: {
+    outputStandalone: true,
+  },
   webpack: (config) => {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
